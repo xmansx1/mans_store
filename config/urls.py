0c1319178ec628fd40bf24a038455b3cf371407a
xmansx1 <your@email.com>
2025-08-20 18:37:31 +0300

    Add cloudinary support

--- config/urls.py
+++ config/urls.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from products.views import landing_page
 from products.views import landing_page, create_sell_request
 
 urlpatterns = [
@@ -11,6 +10,5 @@ urlpatterns = [
     path("sell/", create_sell_request, name="sell_request"),
 ]
 
-# لخدمة الوسائط أثناء التطوير
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
